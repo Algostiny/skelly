90db3c9d4640028c394bb2f4691f2cb3e057586a
Algostiny <augustoschaeferhuff@gmail.com>
2023-12-19 00:46:38 -0300

    ola

---
+++ src/cmds/help.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Ajuda!'),
+    execute: function (interaction, client) {
+        let emb = new EmbedBuilder()
+        .setTitle('Pong 🏓')
+        .setDescription(`Latency - \`${Date.now() - interaction.createdTimestamp}ms\`\nAPI - \`${Math.round(client.ws.ping)}ms\``)
+        .setColor('Purple')
+
+        interaction.reply({ embeds: [emb] })
+    }
+}
